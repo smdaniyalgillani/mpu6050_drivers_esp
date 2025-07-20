b4b186651c395b4d41fa4e11c0efe8d5378c157a
Daniyal <s.daniyalgellani@gmail.com>
2025-07-20 20:59:29 +0500

    Fixed build, facing issues with handle assignment

--- components/comms_i2c/comms_i2c.c
+++ components/comms_i2c/comms_i2c.c
@@ -1,10 +1,8 @@
 #include "comms_i2c.h"
 
-i2c_master_bus_handle_t i2c_bus_handle;
-i2c_master_dev_handle_t mpu_dev_handle;
-
-void i2c_bus_init(int i2c_port_, gpio_num_t scl_, gpio_num_t sda_, bool PULLUP_EN)
+void i2c_bus_init(i2c_bus_controller *i2c_control, i2c_master_bus_handle_t i2c_bus_handle, int i2c_port_, gpio_num_t scl_, gpio_num_t sda_, bool PULLUP_EN)
 {
+    i2c_control->i2c_bus_handle = i2c_bus_handle;
     i2c_master_bus_config_t i2c_bus_conf = {
         .clk_source = I2C_CLK_SRC_DEFAULT,
         .i2c_port = i2c_port_,
@@ -14,10 +12,10 @@ void i2c_bus_init(int i2c_port_, gpio_num_t scl_, gpio_num_t sda_, bool PULLUP_E
         .flags.enable_internal_pullup = PULLUP_EN,    
         };
 
-    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_conf, &i2c_bus_handle));
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_conf, &i2c_control->i2c_bus_handle));
 }
 
-void i2c_add_new_device(i2c_master_dev_handle_t dev_handle, uint16_t dev_addr, uint32_t dev_speed_hz)
+void i2c_add_new_device(i2c_bus_controller *i2c_control, i2c_master_dev_handle_t dev_handle, uint16_t dev_addr, uint32_t dev_speed_hz)
 {
     i2c_device_config_t device_config = {
         .device_address = dev_addr,
@@ -25,6 +23,6 @@ void i2c_add_new_device(i2c_master_dev_handle_t dev_handle, uint16_t dev_addr, u
     };
     
 
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus_handle, &device_config, &dev_handle));
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_control->i2c_bus_handle, &device_config, &dev_handle));
 
 }
--- main/main.c
+++ main/main.c
@@ -14,13 +14,18 @@
 #define mpu_sda GPIO_NUM_5
 #define mpu_int GPIO_NUM_6
 
-i2c_master_bus_handle_t i2c_bus_handle;
-i2c_master_dev_handle_t mpu_dev_handle;
+i2c_master_bus_handle_t *i2c_bus_handle;
+i2c_master_dev_handle_t *mpu_dev_handle;
 mpu6050_dev_t mpu6050;
+i2c_bus_controller i2c_control;
 
 void app_main(void)
 {
 
+    i2c_bus_init(&i2c_control, i2c_bus_handle, 1, mpu_sck, mpu_sda, true);
+    i2c_add_new_device(&i2c_control, mpu_dev_handle, 0x68, 400000);
+    mpu_config(&mpu6050, mpu_dev_handle, ACCEL_16G, GYRO_2000DPS);
+    mpu_start();
     while (1) {
         mpu_read(&mpu6050, ACCELEROMETER);
         vTaskDelay(pdMS_TO_TICKS(1000));
